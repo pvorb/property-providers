f7a75dc5e52a97901d751f1e6cd7ca3b1166ffe0
Paul Vorbach <paul@vorba.ch>
2015-07-10 01:53:07 +0200

    Remove lambda expression to be compatible with 1.7

--- src/main/java/de/vorb/properties/parsers/ValueParsers.java
+++ src/main/java/de/vorb/properties/parsers/ValueParsers.java
@@ -8,15 +8,18 @@ public class ValueParsers {
     private static final Set<String> TRUE_VALUES = new HashSet<>(Arrays.asList("true", "yes", "y", "1"));
     private static final Set<String> FALSE_VALUES = new HashSet<>(Arrays.asList("false", "no", "n", "0"));
 
-    public static final ValueParser<Boolean> BOOLEAN_PARSER = (String value) -> {
-        final String lowerCaseValue = value.toLowerCase();
+    public static final ValueParser<Boolean> BOOLEAN_PARSER = new ValueParser<Boolean>() {
+        @Override
+        public Boolean parseValue(String value) {
+            final String lowerCaseValue = value.toLowerCase();
 
-        if (TRUE_VALUES.contains(lowerCaseValue)) {
-            return Boolean.TRUE;
-        } else if (FALSE_VALUES.contains(lowerCaseValue)) {
-            return Boolean.FALSE;
-        } else {
-            throw new IllegalArgumentException(String.format("Invalid boolean property value '%s'", value));
+            if (TRUE_VALUES.contains(lowerCaseValue)) {
+                return Boolean.TRUE;
+            } else if (FALSE_VALUES.contains(lowerCaseValue)) {
+                return Boolean.FALSE;
+            } else {
+                throw new IllegalArgumentException(String.format("Invalid boolean property value '%s'", value));
+            }
         }
     };
 
