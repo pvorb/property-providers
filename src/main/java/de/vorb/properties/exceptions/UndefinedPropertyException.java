14443e7fc047e85eff7167a9f5d98ce2cac284ea
Paul Vorbach <paul@vorba.ch>
2015-07-19 22:15:51 +0200

    Simplify creation of UndefinedPropertyExceptions

--- src/main/java/de/vorb/properties/exceptions/UndefinedPropertyException.java
+++ src/main/java/de/vorb/properties/exceptions/UndefinedPropertyException.java
@@ -3,11 +3,15 @@ package de.vorb.properties.exceptions;
 public class UndefinedPropertyException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
-    public UndefinedPropertyException(String message) {
-        super(message);
+    public UndefinedPropertyException(String key) {
+        super(errorMessage(key));
     }
 
-    public UndefinedPropertyException(String message, Throwable cause) {
-        super(message, cause);
+    public UndefinedPropertyException(String key, Throwable cause) {
+        super(errorMessage(key), cause);
+    }
+
+    private static String errorMessage(String key) {
+        return String.format("No property found for key '%s'", key);
     }
 }
