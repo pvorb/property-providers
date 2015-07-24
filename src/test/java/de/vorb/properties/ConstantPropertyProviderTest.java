640bf7307ef4109937b42eabffef46301c0a7a4d
Paul Vorbach <paul@vorba.ch>
2015-07-24 18:26:31 +0200

    Add unit test for ConstantPropertyProvider

---
+++ src/test/java/de/vorb/properties/ConstantPropertyProviderTest.java
@@ -0,0 +1,34 @@
+package de.vorb.properties;
+
+import java.util.Properties;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.truth.Truth;
+
+public class ConstantPropertyProviderTest {
+
+    private static final String SOME_OTHER_KEY = "some.other.key";
+
+    private static final String SOME_KEY = "some.key";
+
+    private final Properties testProperties = new Properties();
+    private PropertyProvider constantPropertyProvider;
+
+    @Before
+    public void setUp() {
+        testProperties.setProperty(SOME_KEY, "Some value");
+        testProperties.setProperty(SOME_OTHER_KEY, "Some other value");
+        constantPropertyProvider = PropertyProviders.newConstantProvider(testProperties);
+    }
+
+    @Test
+    public void testGetProperties() {
+        Truth.assertThat(constantPropertyProvider.getProperties().getProperty(SOME_KEY))
+                .isEqualTo(testProperties.getProperty(SOME_KEY));
+        Truth.assertThat(constantPropertyProvider.getProperties().getProperty(SOME_OTHER_KEY))
+                .isEqualTo(testProperties.getProperty(SOME_OTHER_KEY));
+    }
+
+}
